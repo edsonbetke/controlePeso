23f7ec68ea5661d5e4dfd42babcf561376f76ba4
edsonbetke <edsonb@gmail.com>
2021-10-30 09:06:06 -0300

    Projeot criado

---
+++ src/main/java/com/edsonb/controlepeso/ControlePesoApplication.java
@@ -0,0 +1,13 @@
+package com.edsonb.controlepeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edsonb/controlepeso/ControlePesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonb.controlepeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
