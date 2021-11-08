08dd82e7c81328510e274a3accf5f39dd8b34aff
edsonbetke <edsonb@gmail.com>
2021-11-08 13:57:48 -0300

    Inserindo novo Usuario com POST

--- src/main/java/com/edsonb/controlepeso/resources/UsuarioResource.java
+++ src/main/java/com/edsonb/controlepeso/resources/UsuarioResource.java
@@ -1,11 +1,15 @@
 package com.edsonb.controlepeso.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edsonb.controlepeso.domain.Usuario;
 import com.edsonb.controlepeso.services.UsuarioService;
@@ -21,5 +25,12 @@ public class UsuarioResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Usuario obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Usuario obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
