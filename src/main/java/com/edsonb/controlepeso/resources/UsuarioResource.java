e74afa87e2e05f00973e78162058cdabe4eb3cac
edsonbetke <edsonb@gmail.com>
2021-11-10 19:20:30 -0300

    Listando todos os usuarios com DTO

--- src/main/java/com/edsonb/controlepeso/resources/UsuarioResource.java
+++ src/main/java/com/edsonb/controlepeso/resources/UsuarioResource.java
@@ -1,6 +1,8 @@
 package com.edsonb.controlepeso.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edsonb.controlepeso.domain.Usuario;
+import com.edsonb.controlepeso.dto.UsuarioDTO;
 import com.edsonb.controlepeso.services.UsuarioService;
 
 @RestController
@@ -47,5 +50,13 @@ public class UsuarioResource {
 		service.delete(id);
 
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UsuarioDTO>> findAll() {
+		List<Usuario> list = service.finAll();
+		List<UsuarioDTO> listDto = list.stream().map(obj -> new UsuarioDTO(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDto);
 	}
 }
