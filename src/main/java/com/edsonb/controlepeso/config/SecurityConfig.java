24b1ce47574ef530956aeda1e87bd04069778fec
edsonbetke <edsonb@gmail.com>
2021-12-02 17:58:56 -0300

    Implementando autorização

--- src/main/java/com/edsonb/controlepeso/config/SecurityConfig.java
+++ src/main/java/com/edsonb/controlepeso/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.edsonb.controlepeso.security.JWTAuthenticationFilter;
+import com.edsonb.controlepeso.security.JWTAuthorizationFilter;
 import com.edsonb.controlepeso.security.JWTUtil;
 
 @Configuration
@@ -47,7 +48,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
